3778d5fb1afa1d6035c7d8a6caec3c22f0a28f65
Kevin Jarocki <k_j11@hotmail.ca>
2024-04-21 03:14:47 -0400

    Tutorial halfway done

--- tutorial.gd
+++ tutorial.gd
@@ -1,9 +1,10 @@
 extends Control
 
-var buttonPressedCount = 0
+var buttonPressedCount = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MainGameImage.visible = true
+	$TutorialDialog1.visible = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,6 +13,23 @@ func _process(delta):
 
 
 func _on_next_button_pressed():
-	buttonPressedCount += 1
+	$Thwak.play()
+
+	$MainGameImage.visible = false
+	$TutorialDialog1.visible = false
+	if buttonPressedCount == 1:
+		$MolMolImage.visible = true
+		$TutorialDialog2.visible = true
+	if buttonPressedCount == 2:
+		$MolMolImage.visible = false
+		$TutorialDialog2.visible = false
+		$FurnaceImage.visible = true
+		$TutorialDialog3.visible = true
+	if buttonPressedCount == 3:
+		$FurnaceImage.visible = false
+		$TutorialDialog3.visible = false
+		$AnvilImage.visible = true
+		$TutorialDialog4.visible = true
 	
-	pass # Replace with function body.
+	buttonPressedCount += 1
+
